c471ecf4167e72040d9419e4e63e85233dce50ea
Edilson Sousa <edsouzasilv@hotmail.com>
2021-09-29 08:13:23 -0300

    criando o pipe

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -7,12 +7,14 @@ import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
 import { NumeroDirective } from './directives';
 import { ModalCotacaoComponent } from './utils';
+import { DataBrPipe } from './pipes/data-br.pipe';
 
 @NgModule({
   declarations: [
     ConversorComponent,
     NumeroDirective,
-    ModalCotacaoComponent
+    ModalCotacaoComponent,
+    DataBrPipe
   ],
   imports: [
     CommonModule,
